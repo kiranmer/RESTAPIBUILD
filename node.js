ca6463bbfe4e871567552bf87100032e5411fa3f
KiranMer <42990696+kiranmer@users.noreply.github.com>
2019-05-22 12:29:16 +0530

    Create node.js

---
+++ node.js
@@ -0,0 +1,27 @@
+/*
+npm search express
+RUN npm install -y express --save
+RUN npm install -y body-parser --save
+RUN npm install -y cookie-parser --save
+RUN npm install -y multer --save
+*/
+
+var express = require('express');
+var app = express();
+var fs = require("fs");
+
+app.get('/listUsers', function (req, res) {
+   fs.readFile( __dirname + "/" + "data.json", 'utf8', function (err, data) {
+       console.log( data.toString() );
+       res.end( data );
+   });
+})
+
+var server = app.listen(9000, "0.0.0.0",function () {
+
+  var host = server.address().address;
+  var port = server.address().port
+
+  console.log("Example app listening at http://%s:%s",host, port)
+
+})
